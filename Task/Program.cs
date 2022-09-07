5370b28ff751e9a6c665c59a593be5a0a642fd08
Aleksei <ustinov@bmgspb.ru>
2022-09-07 16:35:56 +0300

    Add Task

---
+++ Task/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write("Введите длину массива строк ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[n];
+string[] arrayResult = new string[n];
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write("Строка " + (i + 1) + ": ");
+    array[i] = Console.ReadLine();
+}
+
+Console.WriteLine("Исходный массив строк: ");
+
+for (int i = 0; i < array.Length; i++)
+{  
+     Console.Write($"{array[i]} ");   
+}
+
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if(array[i].Length <= 3)
+    {
+        arrayResult[count] = array[i];
+        count++;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Итоговый массив строк: ");
+
+for (int i = 0; i < arrayResult.Length; i++)
+{  
+     Console.Write($"{arrayResult[i]} ");   
+}
